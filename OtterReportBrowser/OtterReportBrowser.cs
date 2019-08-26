164053442b6bba5637f762fdbd06e46f0fe42efe
wangchaoyu <wangchaoyu@whkdzn.com>
2019-08-26 17:22:43 +0800

    init

---
+++ OtterReportBrowser/OtterReportBrowser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+using CefSharp;
+using OtterReportBrowser.handler;
+using System.ComponentModel;
+
+namespace OtterReportBrowser
+{
+    public partial class OtterReportBrowser : UserControl
+    {
+        public OtterReportBrowser()
+        {
+            if (LicenseManager.UsageMode != LicenseUsageMode.Designtime)
+                InitializeComponent();
+            InitBrowser();
+        }
+
+
+        public void InitBrowser()
+        {
+            //            string url = "http://192.168.0.241:8888/ureport/preview?_u=db:沌口水厂中心调度室运行日报&_t=6";
+            string url = "http://www.baidu.com";
+
+            CefSettings cefSettings = new CefSettings
+            {
+                Locale = "zh-CN",
+                AcceptLanguageList = "zh-CN",
+                MultiThreadedMessageLoop = true
+            };
+
+            Cef.Initialize(cefSettings);
+
+            ChromiumWebBrowser Browser = new ChromiumWebBrowser(Uri.EscapeUriString(url))
+            {
+                Dock = DockStyle.Fill,
+                DownloadHandler = new DownLoaderHandler(),
+                LifeSpanHandler = new LifeSpanHandler()
+            };
+            this.Controls.Add(Browser);
+
+        }
+    }
+}
---
+++ OtterReportBrowser/handler/LifeSpanHandler.cs
@@ -0,0 +1,32 @@
+﻿using CefSharp;
+
+namespace OtterReportBrowser.handler
+{
+    class LifeSpanHandler : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            return true;
+        }
+
+        public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+
+        }
+
+        public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+
+        }
+
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            if (!string.IsNullOrEmpty(targetUrl))
+            {
+                chromiumWebBrowser.Load(targetUrl);
+            }
+            newBrowser = null;
+            return true;
+        }
+    }
+}
